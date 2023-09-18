42e17f25fe77f5fb9ccd1dd25ea35522b2308958
Manthan Trivedi <manthan.trivedi@bito.co>
2023-09-18 19:37:30 +0530

    did some corrections

--- bito-cli-macos-arm.rb
+++ bito-cli-macos-arm.rb
@@ -1,12 +1,12 @@
 class BitoCliLinuxX86 < Formula
-  desc "Bito CLI for Linux x86 based architecure"
+  desc "Bito CLI for MacOS Arm based architecure"
   homepage "https://github.com/gitbito/CLI"
   url "https://github.com/gitbito/CLI/releases/download/vm3.4.0/bito-macos-arm.tar.gz"
   sha256 "0bb392ec76f59286a3e3530529b6b1734f0072ae09ea4dffd45ac9be95554b1d"
   license ""
 
   def install
-    bin.install "bito-linux-x86" => "bito"
+    bin.install "bito-macos-arm" => "bito"
   end
 
   test do
