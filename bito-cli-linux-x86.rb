8fc233c8fb23eddc3d6ae3b1d07219c0e164f641
Manthan Trivedi <manthan.trivedi@bito.co>
2023-09-18 18:34:39 +0530

    test new formula for bito cli

---
+++ bito-cli-linux-x86.rb
@@ -0,0 +1,15 @@
+class BitoCliLinuxX86 < Formula
+  desc "Bito CLI for Linux x86 based architecure"
+  homepage "https://github.com/gitbito/CLI"
+  url "https://github.com/gitbito/CLI/releases/download/v3.4.0/bito-linux-x86.tar.gz"
+  sha256 "25bf6c0dd73e260ac515af85fb8ea465dc79489659824f93c35a5ad1dd870bf5"
+  license ""
+
+  def install
+    bin.install "bito-linux-x86"
+  end
+
+  test do
+    system "#{bin}/bito-linux-x86"
+  end
+end
