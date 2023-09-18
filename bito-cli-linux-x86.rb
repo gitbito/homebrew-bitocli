cf22a1f13ea8050deb9059a4b485744a3898fba0
Manthan Trivedi <manthan.trivedi@bito.co>
2023-09-18 20:31:37 +0530

    changed checksum

--- bito-cli-linux-x86.rb
+++ bito-cli-linux-x86.rb
@@ -2,7 +2,7 @@ class BitoCliLinuxX86 < Formula
   desc "Bito CLI for Linux x86 based architecure"
   homepage "https://github.com/gitbito/CLI"
   url "https://github.com/gitbito/CLI/releases/download/v3.4.0/bito-linux-x86.tar.gz"
-  sha256 "25bf6c0dd73e260ac515af85fb8ea465dc79489659824f93c35a5ad1dd870bf5"
+  sha256 "be9f82619233dae8cbb96c403fb35cc745816e55f121d48ba95923ab4050f093"
   license ""
 
   def install
