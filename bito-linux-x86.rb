ff62c7276b636bf4f77482bde42e6ec290c9463b
Manthan Trivedi <manthan.trivedi@bito.co>
2023-09-26 15:59:53 +0530

    added versioning

--- bito-linux-x86.rb
+++ bito-linux-x86.rb
@@ -1,5 +1,6 @@
 class BitoLinuxX86 < Formula
   desc "Bito CLI for Linux x86 based architecure with new version 3.5 for test"
+  version "3.4.0"
   homepage "https://github.com/gitbito/CLI"
   url "https://github.com/gitbito/CLI/releases/download/v3.4.0/bito-linux-x86.tar.gz"
   sha256 "be9f82619233dae8cbb96c403fb35cc745816e55f121d48ba95923ab4050f093"
