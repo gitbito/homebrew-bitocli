89f935e8304737c1c5ba1e6c240c11796c8fde42
Manthan Trivedi <manthan.trivedi@bito.co>
2023-09-27 15:06:32 +0530

    version change

--- bito-cli-macos-arm.rb
+++ bito-cli-macos-arm.rb
@@ -1,5 +1,6 @@
 class BitoCliMacosArm < Formula
   desc "Bito CLI for MacOS Arm based architecure"
+  version "3.4.0"
   homepage "https://github.com/gitbito/CLI"
   url "https://github.com/gitbito/CLI/releases/download/vm3.4.0/bito-macos-arm.tar.gz"
   sha256 "0bb392ec76f59286a3e3530529b6b1734f0072ae09ea4dffd45ac9be95554b1d"
