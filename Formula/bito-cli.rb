e4c5c7168063cd6939c8c5ab7adeaf78ecc58ed3
Manthan Trivedi <manthan.trivedi@bito.co>
2023-10-04 15:21:34 +0530

    few fixes

--- Formula/bito-cli.rb
+++ Formula/bito-cli.rb
@@ -2,38 +2,37 @@ class BitoCli < Formula
     desc "Bito CLI for Linux x86 based architecure with version 3.1 for test"
     version "3.1.0"
     homepage "https://github.com/gitbito/CLI"
-    url "https://github.com/gitbito/CLI/releases/download/"
     sha256 "5552b20267454b2b959a60a4cda01f060bfa4fe75beef963cec8e5ac6fd5405f"
     license ""
   
     installOS=""
     installArch=""
 
+    # Detect machine OS
+    unos = `uname`.chomp
+
+    ohai "unos: #{unos}"
+
+    if unos == "Darwin"
+        installOS = "macos"
+    elsif unos == "Linux"
+        installOS = "linux"
+    end
+
+    # Detect machine architecture type
+    unmacarch = `uname -m`.chomp
+
+    ohai "unmacarch: #{unmacarch}"
+
+    if unmacarch == "x86_64"
+        installArch = "x86"
+    elsif unmacarch == "arm"
+        installArch = "arm"
+    end
+
+    url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{installOS}-#{installArch}.tar.gz"
+    
     def install
-        # Detect machine OS
-        unos = `uname`.chomp
-        
-        ohai "unos: #{unos}"
-
-        if unos == "Darwin"
-            installOS = "macos"
-        elsif unos == "Linux"
-            installOS = "linux"
-        end
-
-        # Detect machine architecture type
-        unmacarch = `uname -m`.chomp
-
-        ohai "unmacarch: #{unmacarch}"
-
-        if unmacarch == "x86_64"
-            installArch = "x86"
-        elsif unmacarch == "arm"
-            installArch = "arm"
-        end
-
-        url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{installOS}-#{installArch}.tar.gz"
-
       bin.install "bito-#{installOS}-#{installArch}" => "bito"
     end
   
