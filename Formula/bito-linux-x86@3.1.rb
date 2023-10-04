29c950d9699586d0b2053825a79845d1b2cc8727
Manthan Trivedi <manthan.trivedi@bito.co>
2023-10-04 14:14:53 +0530

    corrected scripts

--- Formula/bito-linux-x86@3.1.rb
+++ Formula/bito-linux-x86@3.1.rb
@@ -1,17 +1,13 @@
 class BitoLinuxX86AT31 < Formula
-  desc ""
-  homepage ""
+  desc "Bito CLI for Linux x86 based architecure with version 3.1 for test"
+  version "3.1.0"
+  homepage "https://github.com/gitbito/CLI"
   url "https://github.com/gitbito/CLI/releases/download/v3.1.0/bito-linux-x86.tar.gz"
   sha256 "5552b20267454b2b959a60a4cda01f060bfa4fe75beef963cec8e5ac6fd5405f"
   license ""
 
   def install
-    # Unlink the previous version if it exists
-    rm "#{bin}/bito"
-    ohai "Unlinked previous version: #{old_link}"
-    
     bin.install "bito-linux-x86" => "bito"
-    bin.install_symlink "bito-linux-x86" => "bito"
   end
 
   test do
