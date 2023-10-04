336ffae71a136f14564ce75aa8519e7db9553a02
Manthan Trivedi <manthan.trivedi@bito.co>
2023-10-04 10:00:03 +0530

    added new version 4.0

--- Formula/bito-cli-macos-x86.rb
+++ Formula/bito-cli-macos-x86.rb
@@ -1,9 +1,9 @@
 class BitoCliMacosX86 < Formula
   desc "Bito CLI for MacOS x86 based architecure"
-  version "3.4.0"
+  version "4.0.0"
   homepage "https://github.com/gitbito/CLI"
-  url "https://github.com/gitbito/CLI/releases/download/vmx863.4.0/bito-macos-x86"
-  sha256 "c020b6ffb0213ac3408cd9fe78e089ab93c9f00d2f33131d5cb5c657cdfbcdb2"
+  url "https://github.com/gitbito/CLI/releases/download/vmx864.0.0/bito-macos-x86"
+  sha256 "623ff133f0a6a5e96eba6f82fb644e355d8038214aeb26cb96a4ca089eb8e61b"
   license ""
 
   def install
