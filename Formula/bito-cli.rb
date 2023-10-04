007b28f6cea53906cb6c4433c1cd2c703dfdd969
Manthan Trivedi <manthan.trivedi@bito.co>
2023-10-04 17:35:23 +0530

    added bito cli formulae

--- Formula/bito-cli.rb
+++ Formula/bito-cli.rb
@@ -5,35 +5,55 @@ class BitoCli < Formula
     sha256 "5552b20267454b2b959a60a4cda01f060bfa4fe75beef963cec8e5ac6fd5405f"
     license ""
   
-    @installOS=""
+    installOS=""
     installArch=""
 
     # Detect machine OS
     unos = `uname`.chomp
-
     ohai "unos: #{unos}"
-
     if unos == "Darwin"
-        @installOS = "macos"
+        installOS = "macos"
     elsif unos == "Linux"
-        @installOS = "linux"
+        installOS = "linux"
     end
 
     # Detect machine architecture type
     unmacarch = `uname -m`.chomp
-
     ohai "unmacarch: #{unmacarch}"
-
     if unmacarch == "x86_64"
         installArch = "x86"
     elsif unmacarch == "arm"
         installArch = "arm"
     end
 
-    url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{@installOS}-#{installArch}.tar.gz"
+    url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{installOS}-#{installArch}.tar.gz"
     
     def install
-      bin.install "bito-#{@installOS}-#{installArch}" => "bito"
+
+      # had to rewrite the above code because above variables aren't getting accessed in this function
+
+      installOS=""
+      installArch=""
+  
+      # Detect machine OS
+      unos = `uname`.chomp
+      ohai "unos: #{unos}"
+      if unos == "Darwin"
+          installOS = "macos"
+      elsif unos == "Linux"
+          installOS = "linux"
+      end
+  
+      # Detect machine architecture type
+      unmacarch = `uname -m`.chomp  
+      ohai "unmacarch: #{unmacarch}"
+      if unmacarch == "x86_64"
+          installArch = "x86"
+      elsif unmacarch == "arm"
+          installArch = "arm"
+      end
+  
+      bin.install "bito-#{installOS}-#{installArch}" => "bito"
     end
   
     test do
