9eb0336e2bfb40065f4d21e2a6ef88f1b729bd60
Manthan Trivedi <manthan.trivedi@bito.co>
2023-10-04 15:45:07 +0530

    added fixes

--- Formula/bito-cli.rb
+++ Formula/bito-cli.rb
@@ -4,38 +4,36 @@ class BitoCli < Formula
     homepage "https://github.com/gitbito/CLI"
     sha256 "5552b20267454b2b959a60a4cda01f060bfa4fe75beef963cec8e5ac6fd5405f"
     license ""
+  
+    installOS=""
+    installArch=""
 
-    def initialize
-      @installOS=""
-      @installArch=""
+    # Detect machine OS
+    unos = `uname`.chomp
 
-      # Detect machine OS
-      unos = `uname`.chomp
+    ohai "unos: #{unos}"
 
-      ohai "unos: #{unos}"
-
-      if unos == "Darwin"
-          @installOS = "macos"
-      elsif unos == "Linux"
-          @installOS = "linux"
-      end
-
-      # Detect machine architecture type
-      unmacarch = `uname -m`.chomp
-
-      ohai "unmacarch: #{unmacarch}"
-
-      if unmacarch == "x86_64"
-          @installArch = "x86"
-      elsif unmacarch == "arm"
-          @installArch = "arm"
-      end
-
-      url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{@installOS}-#{@installArch}.tar.gz"
+    if unos == "Darwin"
+        installOS = "macos"
+    elsif unos == "Linux"
+        installOS = "linux"
     end
-      
+
+    # Detect machine architecture type
+    unmacarch = `uname -m`.chomp
+
+    ohai "unmacarch: #{unmacarch}"
+
+    if unmacarch == "x86_64"
+        installArch = "x86"
+    elsif unmacarch == "arm"
+        installArch = "arm"
+    end
+
+    url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{installOS}-#{installArch}.tar.gz"
+    
     def install
-      bin.install "bito-#{@installOS}-#{@installArch}" => "bito"
+      bin.install "bito-#{installOS}-#{installArch}" => "bito"
     end
   
     test do
