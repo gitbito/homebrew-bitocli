982337266a658e93d88f4c9a8c96de78797351bc
Manthan Trivedi <manthan.trivedi@bito.ai>
2024-03-20 10:40:46 +0530

    Bito CLI Version 4.0

--- Formula/bito-cli.rb
+++ Formula/bito-cli.rb
@@ -1,6 +1,6 @@
 class BitoCli < Formula
-  desc "Bito CLI Version 3.9"
-  version "3.9.0"
+  desc "Bito CLI Version 4.0"
+  version "4.0.0"
   homepage "https://github.com/gitbito/CLI"
   license ""
 
@@ -32,23 +32,49 @@ class BitoCli < Formula
    installOS, installArch = self.set_arch_and_os
 
    if installOS == "linux" && installArch == "x86"
-    sha256 "776861ffb66a700f6d3e918c87c9e9f0b3dec67df36c291d30dfd38a882b5d5a"
+    sha256 "c910360376568c805f0fbf426bd35ffef6b2692205c5c8ca120f91bff7a4bbc0"
   elsif installOS == "macos" && installArch == "x86"
-    sha256 "042569f032cc0d317ad001d216d81e31647c052c956f5d40ad2e192177d660cd"
+    sha256 "2b3ca104ab987d24c6a47386db8ae9405450cbe6671cce717298ca9c4b3b2dac"
   elsif installOS == "linux" && installArch == "arm"
-    sha256 "29bbf70fd691ae02d4a065e6284d002c6707aae8391e94516b65a5994ea67858"
+    sha256 "5794a0c9ad9dd919901e2e6943a56f21f3f9d11abdf368b2c5926b49cb55dab6"
   elsif installOS == "macos" && installArch == "arm"
-    sha256 "33480d805fde6994acfaea924224282e899375b557da8fccb453224043185e11"
+    sha256 "af5fa370ed2f1062d8b8345b0def67e98527bfe565995417bd17842805aeda2a"
   end
 
    url "https://github.com/gitbito/CLI/releases/download/v#{version}/bito-#{installOS}-#{installArch}.tar.gz"
    
    def install
     installOS, installArch = self.class.set_arch_and_os
+
+    puts("Post installation script")
+
+    lca_binary = "bito-lca"
+    supported_files = "bito-slashCommands.json"
+    lca_bundle = "bito-lca-#{installOS}.tar.gz"
+
+    lca_bundle_url = "https://github.com/gitbito/CLI/releases/download/packages/#{lca_bundle}"
+    supported_files_url = "https://github.com/gitbito/CLI/releases/download/packages/#{supported_files}"
+
+    lca_os_specific = "bito-lca-#{installOS}"
+    prefix = "/usr/local/bin"
+
+    # Create the directory if it doesn't exist
+    FileUtils.mkdir_p(prefix) unless Dir.exist?(prefix)
+    # Change the permissions of the directory and all its contents
+      
+    # Use curl to download files
+    system "curl", "-L", "#{lca_bundle_url}", "-o", "#{lca_bundle}"
+    system "curl", "-L", "#{supported_files_url}", "-o", "#{supported_files}"
+
+    # Unpack the lca_bundle tar.gz file
+    system "tar", "-xzf", "#{lca_bundle}"
+  
     bin.install "bito-#{installOS}-#{installArch}" => "bito"
+    bin.install "#{lca_os_specific}" => "#{lca_binary}"
+    bin.install "#{supported_files}" => "#{supported_files}"
   end
 
-   test do
+  test do
     system "#{bin}/bito"
   end
 end
